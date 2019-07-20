f463980aedbecc8445131274ceb8c5057d66b0b6
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-07-20 11:39:42 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>hehehehehhehehehe</h1>"
+
+app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
---
+++ readMe.md
@@ -0,0 +1,3 @@
+# chit-chApp
+
+I'm following a walkthrough on Flask to make a chat app. The purpose of the app is to take data from a url and store it in a list.
