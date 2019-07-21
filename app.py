2f0cf4ecf90d05de914ae908a199067f363f1b05
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-07-21 10:42:39 +0100

    added functionality to show messages by adding them to a list and then redirecting back to user page and dispkaying them

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_messages(username, message):
+    """add messages to messages list"""
+    messages.append("{}: {}".format(username, message))
+
+def get_all_messages():
+    """Get all messages and seperate them with a `br`"""
+    return "<br>".join(messages)
 
 @app.route('/')
 def index():
@@ -11,10 +19,13 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """display messages"""
+    return "<h1>Welcome, {0}</h1> {1} ".format(username, get_all_messages())
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """create a message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
